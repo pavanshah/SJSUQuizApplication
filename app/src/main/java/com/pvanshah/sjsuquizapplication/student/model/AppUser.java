3529e2e2a71532f922c4ce8b35d1374a395f096f
Avinash Ravilla <avinash@Avinashs-MacBook-Air.local>
2017-08-27 18:19:38 -0700

    user object model

---
+++ app/src/main/java/com/pvanshah/sjsuquizapplication/student/model/AppUser.java
@@ -0,0 +1,27 @@
+package com.pvanshah.sjsuquizapplication.student.model;
+
+/**
+ * Created by avinash on 8/27/17.
+ */
+
+public class AppUser {
+
+    private String username;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
